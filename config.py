06450e0a474c937d5c12fe6768da9b9cee8be177
Bastien-MOINS <bastienmoins@gmail.com>
2025-09-09 16:59:14 +0200

    TP2 commit entier

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ ABOUT = "Bienvenue sur la page à propos de Flask !"
 CONTACT = "Vous pouvez me retrouver sur \n" \
             "Facebook: https://www.facebook.com/groups/1209157305791416/ \n" \
             "Flask's documentation: https://flask.palletsprojects.com/en/stable/"
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'monApp.db')
--- monApp/app.py
+++ monApp/app.py
@@ -6,3 +6,8 @@ app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
 app.config['ABOUT']
 app.config['CONTACT']
+
+# Create database connection object
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
+#db.init_app(app)
